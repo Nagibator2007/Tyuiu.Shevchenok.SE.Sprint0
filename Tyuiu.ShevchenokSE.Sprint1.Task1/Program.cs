6a78d92cb00d5742f21e95196a9f6e49ebcaa217
Nagibator2007 <shevchenokse@outlook.com>
2025-10-01 22:36:20 +0500

    Таск 1 добавлен!

---
+++ Tyuiu.ShevchenokSE.Sprint1.Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShevchenokSE.Sprint1.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Sergey";
+            int age = 18;
+            Console.WriteLine("Hello, my name is " +  name  + " im " +  age + " years old!");
+            Console.ReadKey();
+        }
+    }
+}
