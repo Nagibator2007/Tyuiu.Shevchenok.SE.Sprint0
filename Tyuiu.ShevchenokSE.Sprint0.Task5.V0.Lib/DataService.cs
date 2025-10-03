1a16dd712d9cd1523203b69f8b40a0a9e49eaa98
Nagibator2007 <shevchenokse@outlook.com>
2025-10-03 21:45:58 +0500

    Task 5 Added!!

---
+++ Tyuiu.ShevchenokSE.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ShevchenokSE.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        { return a + b; }
+        public static int Substraction(int a, int b)
+        { return a - b; }
+        public static int Multiplication(int a, int b)
+        { return a * b; }
+        public static int Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("What tf are u writing man? its impossible to Division when b= 0!");
+                return -1;
+            }
+            else
+            { return a / b; }
+        }
+       
+    }
+
+
+}
