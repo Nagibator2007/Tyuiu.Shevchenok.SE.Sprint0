9e5d3825c980b9a9563741bc7adbdcbfc58600ed
Nagibator2007 <shevchenokse@outlook.com>
2025-10-03 23:45:03 +0500

    Task 6 Added!!

---
+++ Tyuiu.ShevchenokSE.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShevchenokSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShevchenokSE.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f09b4d6a-57af-43a8-8ad4-01b51e291749")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShevchenokSE.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShevchenokSE.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.ShevchenokSE.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Summ of elements equals " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Summ of elements equals " + DataService.SubstractionArray(numsArray));
+            Console.WriteLine("Summ of elements equals " + DataService.MultiplicationArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
