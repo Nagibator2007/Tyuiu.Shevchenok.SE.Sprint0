85aedab9ac3ce2e9d1369ae5590994c70b885fdc
Nagibator2007 <shevchenokse@outlook.com>
2025-10-03 21:08:00 +0500

    Added Task4!

---
+++ Tyuiu.ShevchenokSE.Sprint0.Task4.V0.Teat/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShevchenokSE.Sprint0.Task4.V0.Teat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShevchenokSE.Sprint0.Task4.V0.Teat")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bdd14bf-d4b3-4a85-a5aa-11b280a5a509")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShevchenokSE.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShevchenokSE.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.ShevchenokSE.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Substraction(3, 5));
+            Console.WriteLine(DataService.Multiplication(6, 5));
+            Console.WriteLine(DataService.Division(25, 5));
+            Console.ReadKey();
+        }
+    }
+}
